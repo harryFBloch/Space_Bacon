2254ea430aea740a6c462ef3ec73d56dcd5c6288
harryFBloch <harryfbloch@gmail.com>
2020-02-12 13:33:06 -0500

    add hrefs to links on home banner

--- src/components/BannerImage.js
+++ src/components/BannerImage.js
@@ -8,8 +8,14 @@ export const BannerImage = (props) => {
     <img src={props.src} alt={props.alt} className={classes.banner}/>
      {props.buttons && 
       <div className={classes.bannerButtonContainer}>
-        <button className={classes.bannerButton}>Join Our Cult</button>
-        <button className={`${classes.bannerButton} ${classes.secondButton}`}>Spotify</button>
+        <a href="https://www.facebook.com/spacebaconmusic/" alt="fan page"  target="_blank" rel="noopener noreferrer">
+          <button className={classes.bannerButton}>Join Our Cult</button>
+        </a>
+        
+        <a href="https://open.spotify.com/album/63AhtrFcqNadzO0nhgWZrB" 
+          alt="spotify music"  target="_blank" rel="noopener noreferrer">
+          <button className={`${classes.bannerButton} ${classes.secondButton}`}>Stream</button>
+        </a>
       </div>
     }
     </div>
